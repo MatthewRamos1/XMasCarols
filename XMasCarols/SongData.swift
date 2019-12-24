93179642e5d080b1b4e6ef6b70d7e597c5c00b0e
Matthew Ramos <mmramos309@gmail.com>
2019-12-23 23:24:35 -0500

    Added song struct data

--- XMasCarols/SongData.swift
+++ XMasCarols/SongData.swift
@@ -49,7 +49,35 @@ We wish you a Merry Christmas
 We wish you a Merry Christmas
 We wish you a Merry Christmas and a Happy New Year!
 """, id: 0),
-    Song(name: "Deck the Halls", lyrics: "", id: 1),
+    Song(name: "Deck the Halls", lyrics:
+        """
+        Deck the halls with boughs of holly
+        Fa-la-la-la-la, la-la-la-la
+        'Tis the season to be jolly
+        Fa-la-la-la-la, la-la-la-la
+        Don we now our gay apparel
+        Fa-la-la, la-la-la, la-la-la
+        Troll the ancient Yule-tide carol
+        Fa-la-la-la-la, la-la-la-la
+
+        See the blazing Yule before us
+        Fa-la-la-la-la, la-la-la-la
+        Strike the harp and join the chorus
+        Fa-la-la-la-la, la-la-la-la
+        Follow me in merry measure
+        Fa-la-la, la-la-la, la-la-la
+        While I tell of Yule-tide treasure
+        Fa-la-la-la-la, la-la-la-la
+
+        Fast away the old year passes
+        Fa-la-la-la-la, la-la-la-la
+        Hail the new year, lads and lasses
+        Fa-la-la-la-la, la-la-la-la
+        Sing we joyous, all together
+        Fa-la-la, la-la-la, la-la-la
+        Heedless of the wind and weather
+        Fa-la-la-la-la, la-la-la-la
+        """, id: 1),
     Song(name: "Song 3", lyrics: "", id: 2)
     ]
 }
