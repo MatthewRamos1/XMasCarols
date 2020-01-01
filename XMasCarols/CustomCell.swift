d59c3587310bd4948d52cbec45da480956040ef7
Matthew Ramos <mmramos309@gmail.com>
2019-12-31 22:29:16 -0500

    Changed cell type to custom, added image view with opaque alpha value for a visual overlaying effect, reformatted label

---
+++ XMasCarols/CustomCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomCell.swift
+//  XMasCarols
+//
+//  Created by Matthew Ramos on 12/31/19.
+//  Copyright © 2019 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+ 
+}
