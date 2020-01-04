78eba65efcbff101c06af5f0437d86b144c392b4
Matthew Ramos <mmramos309@gmail.com>
2020-01-04 18:02:36 -0500

    Added various local Song data

--- XMasCarols/SongData.swift
+++ XMasCarols/SongData.swift
@@ -14,8 +14,7 @@ struct Song {
     let id: Int
     
     
-    static let songs: [Song] = [Song(name: "We Wish You a Merry Christmas", lyrics:
-        """
+    static let songs: [Song] = [Song(name: "We Wish You a Merry Christmas", lyrics: """
 We wish you a Merry Christmas
 We wish you a Merry Christmas
 We wish you a Merry Christmas and a Happy New Year
@@ -49,8 +48,7 @@ We wish you a Merry Christmas
 We wish you a Merry Christmas
 We wish you a Merry Christmas and a Happy New Year!
 """, id: 0),
-    Song(name: "Deck the Halls", lyrics:
-        """
+    Song(name: "Deck the Halls", lyrics: """
         Deck the halls with boughs of holly
         Fa-la-la-la-la, la-la-la-la
         'Tis the season to be jolly
@@ -78,9 +76,104 @@ We wish you a Merry Christmas and a Happy New Year!
         Heedless of the wind and weather
         Fa-la-la-la-la, la-la-la-la
         """, id: 1),
-    Song(name: "Song 3", lyrics: "", id: 2),
-    Song(name: "", lyrics: "", id: 3),
-    Song(name: "", lyrics: "", id: 4),
+    Song(name: "Frosty the Snowman", lyrics: """
+Frosty the snowman was a jolly happy soul
+With a corncob pipe and a button nose
+And two eyes made out of coal
+Frosty the snowman is a fairy tale, they say
+He was made of snow but the children know
+How he came to life one day
+There must have been some magic in
+That old silk hat they found
+For when they placed it on his head
+He began to dance around
+Oh, Frosty the snowman
+Was alive as he could be
+And the children say he could laugh and play
+Just the same as you and me
+
+Thumpety thump thump
+Thumpety thump thump
+Look at Frosty go
+Thumpety thump thump
+Thumpety thump thump
+Over the hills of snow
+
+Ooh Frosty the snowman
+Knew the sun was hot that day
+So he said, "Let's run and we'll have some fun
+Now before I melt away."
+Down to the village
+With a broomstick in his hand
+Running here and there all around the square
+Saying "catch me if you can!"
+He led them down the streets of town
+Right to the traffic cop
+And he only paused a moment when
+He heard him holler "Stop!"
+Oh, Frosty the snowman
+Had to hurry on his way
+But he waved goodbye, saying
+"Don't you cry, I'll be back again someday."
+
+Thumpety thump thump
+Thumpety thump thump
+Look at Frosty go
+Thumpety thump thump
+Thumpety thump thump
+Over the hills of snow!
+""", id: 2),
+    Song(name: "O Holy Night", lyrics: """
+O Holy night! The stars are brightly shining
+It is the night of our dear Savior's birth
+Long lay the world in sin and error pining
+'Til He appears and the soul felt its worth
+A thrill of hope the weary world rejoices
+For yonder breaks a new and glorious morn
+Fall on your knees; O hear the Angel voices!
+O night divine, O night when Christ was born
+O night, O Holy night, O night divine!
+
+Led by the light of Faith serenely beaming
+With glowing hearts by His cradle we stand
+So led by light of a star sweetly gleaming
+Here come the Wise Men from Orient land
+The King of kings lay thus in lowly manger
+In all our trials born to be our friend
+He knows our need, to our weakness is no stranger
+Behold your King; before Him lowly bend
+Behold your King; before Him lowly bend
+
+Truly He taught us to love one another;
+His law is love and His Gospel is Peace
+Chains shall He break, for the slave is our brother
+And in His name, all oppression shall cease
+Sweet hymns of joy in grateful chorus raise we
+Let all within us Praise His Holy name
+Christ is the Lord; O praise His name forever!
+His power and glory evermore proclaim
+His power and glory evermore proclaim, id: 3)
+""", id: 3),
+    Song(name: "Have Yourself a Merry Little Christmas", lyrics: """
+Have yourself a merry little Christmas
+Let your heart be light
+Next year all our troubles will be out of sight
+
+Have yourself a merry little Christmas
+Make the Yuletide gay
+Next year all our troubles will be miles away
+
+Once again as in olden days
+Happy golden days of yore
+Faithful friends who are dear to us
+Will be near to us, once more
+
+Someday soon we all will be together
+If the fates allow
+Until then we'll have to muddle through somehow
+
+So have yourself a merry little Christmas now
+""", id: 4),
     Song(name: "", lyrics: "", id: 5),
     Song(name: "", lyrics: "", id: 6),
     Song(name: "", lyrics: "", id: 7),
