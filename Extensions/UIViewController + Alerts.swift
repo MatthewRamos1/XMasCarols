d960720b4fb76c4317482013392f171b9191ab16
Matthew Ramos <mmramos309@gmail.com>
2020-01-05 22:02:44 -0500

    Added alert extension.

---
+++ Extensions/UIViewController + Alerts.swift
@@ -0,0 +1,18 @@
+//
+//  UIViewController + Alerts.swift
+//  XMasCarols
+//
+//  Created by Matthew Ramos on 1/5/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+  func showAlert(title: String, message: String) {
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+    alertController.addAction(okAction)
+    present(alertController, animated: true, completion: nil)
+  }
+}
